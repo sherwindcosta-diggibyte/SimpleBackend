01dce603de51206f92170879b90d85e3d845455a
Sherwin Dcosta <sherwin.dcosta@diggibyte.com>
2023-03-08 15:49:29 +0530

    added port

--- server.js
+++ server.js
@@ -13,3 +13,8 @@ app.use(cors(corsOptions));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the React application " });
 });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
